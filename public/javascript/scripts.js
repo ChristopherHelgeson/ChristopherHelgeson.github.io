b1f2839b1aba1a4f8adec4a7c02bf08e7888afec
Christopher Helgeson <ChristopherHelgeson@hotmail.com>
2017-11-08 12:13:08 -0500

    sync clock updates

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,59 +1,28 @@
 var header = document.getElementsByTagName("h1")[0];
 header.innerHTML = "JavaScript FTW!";
 
-//CLOCK SCRIPT FOLLOWS:
-const currentDate = new Date();
-const date = currentDate.getDate();
-const month = currentDate.getMonth();
-const year = currentDate.getFullYear();
-const monthNames = [
-  "January", "February", "March", "April", "May", "June",
-   "July", "August", "September", "October", "November", "December"
-]
+const timeAtLoad = moment();
 
-const dateString = date + " " + monthNames[month] + " " + year;
-
+// Set the date string using formatting provided by moment.js
+const dateString = timeAtLoad.format("dddd, MMMM Do, YYYY");
 document.getElementById("current-date").textContent = dateString;
 
-// function startTime() {
-//
-//   const now = new Date();
-//
-//   let hours = now.getHours();
-//   const meridiem = hours >= 12 ? "pm" : "am";
-//   hours = hours % 12;
-//   hours = hours ? hours : 12;
-//   hours = hours < 10 ? "0" + hours : hours;
-//
-//   let minutes = now.getMinutes();
-//   minutes = minutes < 10 ? "0" + minutes : minutes;
-//
-//   let seconds = now.getSeconds();
-//   seconds = seconds < 10 ? "0" + seconds : seconds;
-//
-//   const timeString = hours + ":" + minutes + ":" + seconds + " " + meridiem;
-//
-//   document.getElementById("current-time").textContent = timeString;
-//
-//   const timeout = setTimeout(startTime, 500);
-// }
-//
-// startTime();
+function startTime() {
 
-//HERO CLOCK FROM MOMENT.JS
-function updateClock() {
-		        var now = moment(),
-		            second = now.seconds() * 6,
-		            minute = now.minutes() * 6 + second / 60,
-		            hour = ((now.hours() % 12) / 12) * 360 + 90 + minute / 12;
-		        $('#hour').css("transform", "rotate(" + hour + "deg)");
-		        $('#minute').css("transform", "rotate(" + minute + "deg)");
-		        $('#second').css("transform", "rotate(" + second + "deg)");
-		    }
+  //digital clock
+  const now = moment();
+  const timeString = now.format("hh:mm:ss a");
+  document.getElementById("current-time").textContent = timeString;
 
-function timedUpdate () {
-    updateClock();
-    setTimeout(timedUpdate, 1000);
+  //analog clock
+  var second = now.seconds() * 6,
+      minute = now.minutes() * 6 + second / 60,
+      hour = ((now.hours() % 12) / 12) * 360 + 90 + minute / 12;
+  document.getElementById("hour").style.transform = "rotate(" + hour + "deg)";
+  document.getElementById("minute").style.transform = "rotate(" + minute + "deg)";
+  document.getElementById("second").style.transform = "rotate(" + second + "deg)";
+
+  const timeout = setTimeout(startTime, 500);
 }
 
-timedUpdate();
+startTime();
